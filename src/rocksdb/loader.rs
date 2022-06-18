e147ef83b26efe46032d521dafaff243316d93e8
Binh Vu <binh@toan2.com>
2022-06-17 22:09:29 -0700

    testing loader

--- src/rocksdb/loader.rs
+++ src/rocksdb/loader.rs
@@ -1,6 +1,8 @@
 use rocksdb::{DBWithThreadMode, Options, DB};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
+use rayon::prelude::*;
+use flate2::read::GzDecoder;
 
 /// Extracting key and value from an object.
 struct KVExtractor {
@@ -35,11 +37,41 @@ enum FileFormat {
 /// * `files` - path to the files to load into RocksDB
 /// * `format` - file format
 pub fn load(dbpath: &Path, dbopts: &Options, files: &[&Path], format: FileFormat, verbose: bool) {
-    let db = DBWithThreadMode<MultiThreaded>::open(dbopts, dbpath);
-    load_into_db(db, files, format, verbose)
+    let db = DBWithThreadMode<MultiThreaded>::open(dbopts, dbpath)?;
+
+    files.iter()
+        .map(|file| {
+            build_sst_file(file, format);
+    //         // let mut writer = SstFileWriter::create(dbopts);
+    //         // writer.open(dbpath)?;
+
+    //         // read the file
+    //         match format {
+    //             FileFormat::TabSep => {
+
+    //             },
+    //             _ => unimplemented! ()
+    //         }
+            
+    //         // writer.finish()?;
+        });
 }
 
-/// Load files into RocksDB
-pub fn load_into_db(db: DBWithThreadMode<MultiThreaded>, files: &[&Path], format: FileFormat, verbose: bool) {
-    
+pub fn build_sst_file(file: &Path, format: &FileFormat) {
+    let reader = if file.extension() == "gz" {
+        BufReader::new(GzDecoder::new(file))
+    } else {
+        BufReader::new(File::open(file)?)
+    };
+
+    match format {
+        FileFormat::TabSep => {
+            for line in reader.lines() {
+                let kv = line.split("\t", 1);
+                println!(kv);
+            }
+        },
+        _ => unimplemented! ()
+    }
 }
+
