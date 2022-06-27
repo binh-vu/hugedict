5aa943ab820713d62eeb96ed4d1196afa4650a31
Binh Vu <binh@toan2.com>
2022-06-26 19:10:14 -0700

    update ci

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -67,7 +67,7 @@ echo "::endgroup::"
 echo
 
 echo "::group::Discovering Python"
-IFS=',' read -a PYTHON_HOMES <<< $(MINIMUM_PYTHON_VERSION=3.7 python $SCRIPT_DIR/pydiscovery.py)
+IFS=',' read -a PYTHON_HOMES <<< $(MINIMUM_PYTHON_VERSION=3.8 python $SCRIPT_DIR/pydiscovery.py)
 if [ ${#PYTHON_HOMES[@]} -eq 0 ]; then
     echo "No Python found. Did you forget to set any environment variable PYTHON_HOME or PYTHON_HOMES?"
 else
