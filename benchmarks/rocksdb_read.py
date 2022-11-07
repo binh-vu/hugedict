e13c20fe0ba3ba30c215583c1362e23afd48119f
Binh Vu <binh@toan2.com>
2022-11-06 16:12:47 -0800

    update code to use latest sem-desc

--- benchmarks/rocksdb_read.py
+++ benchmarks/rocksdb_read.py
@@ -4,20 +4,17 @@
 from functools import partial
 import random
 from loguru import logger
-from matplotlib.style import available
 import orjson
-from dataclasses import dataclass
-import multiprocessing
-import shutil
 from pathlib import Path
-from typing import Optional
-
-from hugedict_v1.loader import FileFormat, load
+import serde.textline
+from timer import Timer
 from rocksdb import DB, Options  # type: ignore
-from sm.prelude import M
 
 from hugedict_v1.rocksdb import RocksDBDict
-from hugedict.hugedict.rocksdb import RocksDBDict as RustRocksDBDict, Options
+from hugedict.hugedict.rocksdb import (
+    RocksDBDict as RustRocksDBDict,
+    Options as RustOptions,
+)
 
 bench_dir = Path(__file__).parent
 
@@ -27,30 +24,30 @@ if not (bench_dir / "tempdir" / "keys.txt.gz").exists():
     )
 
     keys = list(db.keys())
-    M.serialize_lines(keys, bench_dir / "tempdir" / "keys.txt.gz")
+    serde.textline.ser(keys, bench_dir / "tempdir" / "keys.txt.gz")
     logger.info("Stop after creating list of keys. You need to rerun the script")
     exit(0)
 
-keys = M.deserialize_lines(bench_dir / "tempdir" / "keys.txt.gz", trim=True)
+keys = serde.textline.deser(bench_dir / "tempdir" / "keys.txt.gz", trim=True)
 random.shuffle(keys)
 keys = keys[:3000]
 
 db = RocksDBDict(
     str(bench_dir / "tempdir" / "python-rocksdb.db"), deser_value=orjson.loads
 )
-with M.Timer().watch_and_report("python-rocksdb"):
+with Timer().watch_and_report("python-rocksdb"):
     count = 0
     for id in keys:
         count += len(db[id])
 
 db2 = RustRocksDBDict(
     path=str(bench_dir / "tempdir" / "rust-rocksdb.db"),
-    options=Options(),
+    options=RustOptions(),
     deser_key=partial(str, encoding="utf-8"),
     deser_value=orjson.loads,
     ser_value=orjson.dumps,
 )
-with M.Timer().watch_and_report("rust-rocksdb"):
+with Timer().watch_and_report("rust-rocksdb"):
     count = 0
     for id in keys:
         count += len(db2[id])
