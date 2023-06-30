bff3906ab1d803974973f92c0e3dbe304d47c815
Binh Vu <binh@toan2.com>
2023-06-30 09:05:26 -0700

    add sqlitedict cache function wrapper

--- hugedict/sqlitedict.py
+++ hugedict/sqlitedict.py
@@ -1,19 +1,28 @@
 from __future__ import annotations
 
-from enum import Enum
+import functools
+import pickle
 import sqlite3
+from enum import Enum
+from inspect import signature
 from pathlib import Path
 from typing import (
+    Any,
     Callable,
     Iterable,
     Iterator,
-    Union,
-    TypeVar,
+    Literal,
+    Optional,
     Tuple,
+    TypeVar,
+    Union,
 )
-from hugedict.cachedict import CacheDict
-from hugedict.types import HugeMutableMapping, V
 
+import orjson
+from timer import Timer
+
+from hugedict.cachedict import CacheDict
+from hugedict.types import F, HugeMutableMapping, V
 
 SqliteKey = TypeVar("SqliteKey", bound=Union[str, int, bytes])
 
@@ -137,3 +146,90 @@ class SqliteDict(HugeMutableMapping[SqliteKey, V]):
 
     def cache(self) -> CacheDict:
         return CacheDict(self)
+
+    @staticmethod
+    def cache_fn(
+        ser: Callable[[Any], bytes] = pickle.dumps,
+        deser: Callable[[bytes], Any] = pickle.loads,
+        cache_args: Optional[list[str]] = None,
+        cache_key: Optional[Callable[..., bytes]] = None,
+        outfile: Optional[Union[str, Path]] = None,
+        log_serde_time: bool = False,
+        disable: bool = False,
+    ) -> Callable[[F], F]:
+        """Decorator to cache the result of a function to a file using sqlitedict.
+        Note: It does not support function with variable number of arguments.
+
+        Args:
+            ser: A function to serialize the output of the function to bytes.
+            deser: A function to deserialize the output of the function from bytes.
+            cache_args: list of arguments to use for the default cache key function. If None, all arguments are used. If cache_key is provided
+                this argument is ignored.
+            cache_key: Function to use to generate the cache key. If None, the default is used. The default function
+                only support arguments of types str, int, bool, and None.
+            outfile: where to store the cache file. If None, the name of the function is used and the location of the file is in /tmp. If it is a function,
+                it will be called with the arguments of the function to generate the filename.
+            log_serde_time: if True, will log the time it takes to deserialize the cache file.
+            disable: if True, the cache is disabled.
+        """
+        if isinstance(disable, bool) and disable:
+            return lambda x: x
+
+        def wrapper_fn(func):
+            if outfile is None:
+                dbpath = Path("/tmp") / f"{func.__name__}.sqlite"
+            else:
+                dbpath = outfile
+
+            keyfn = cache_key
+            if keyfn is None:
+                fnargs = {}
+                for name, param in signature(func).parameters.items():
+                    fnargs[name] = param
+                fnargnames = list(fnargs.keys())
+
+                def default_keyfn(*args, **kwargs):
+                    out = {name: value for name, value in zip(fnargs, args)}
+                    out.update(
+                        [
+                            (name, kwargs.get(name, fnargs[name].default))
+                            for name in fnargnames[len(args) :]
+                        ]
+                    )
+                    if cache_args is not None and len(cache_args) != len(fnargnames):
+                        out = {name: out[name] for name in cache_args}
+                    return orjson.dumps(out)
+
+                keyfn = default_keyfn
+
+            sqlitedict: SqliteDict[bytes, bytes] = SqliteDict(
+                dbpath,
+                SqliteDictFieldType.bytes,
+                lambda x: x,
+                lambda x: x,
+                timeout=30,
+            )
+
+            @functools.wraps(func)
+            def fn(*args, **kwargs):
+                key = keyfn(*args, **kwargs)
+                if key not in sqlitedict:
+                    output = func(*args, **kwargs)
+                    if log_serde_time:
+                        with Timer().watch_and_report("serialize value"):
+                            sqlitedict[key] = ser(output)
+                    else:
+                        sqlitedict[key] = ser(output)
+                else:
+                    if log_serde_time:
+                        with Timer().watch_and_report(
+                            "deserialize value",
+                        ):
+                            output = deser(sqlitedict[key])
+                    else:
+                        output = deser(sqlitedict[key])
+                return output
+
+            return fn
+
+        return wrapper_fn  # type: ignore
